50006cf005e7035ff5334df9079a741ba14272fe
Sam <sam@Sams-MacBook-Pro.local>
2022-03-04 23:18:08 -0500

    redirect our-work

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ module.exports = {
   images: {
     domains: ['media.graphcms.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/our-work',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
