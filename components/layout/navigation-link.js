3afca86f8c3f33a3644a09423b8771f87d4f2536
Sam <sam@Sams-MacBook-Pro.local>
2022-03-05 09:49:30 -0500

    add class for tracking

--- components/layout/navigation-link.js
+++ components/layout/navigation-link.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NavigationLink({ href, text }) {
   return (
     <Link href={href} passHref>
-      <a className="text-blue-900 ml-8 text-sm md:text-lg font-bold tracking-wide uppercase transition-colors hover:text-blue-700">
+      <a className="text-blue-900 ml-8 text-sm md:text-lg font-bold tracking-wide uppercase transition-colors hover:text-blue-700 menu-link--top">
         {text}
       </a>
     </Link>
